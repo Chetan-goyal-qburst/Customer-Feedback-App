456e9dab18809480f634b9961d6e1fdce2b419be
Chetan-goyal-qburst <chetan.goyal@qburst.com>
2023-08-02 14:38:13 +0530

    update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ import "./header.css";
 const Header = (props) => {
   return (
     <div className="heading">
-      <h2 className="heading-text">{props.name}</h2>
+      <h2 className="heading-pop">{props.name}</h2>
     </div>
   );
 };

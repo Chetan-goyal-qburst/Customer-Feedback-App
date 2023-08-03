9dc52007d88f420c8e8a96a745140bf4b0e451de
rajendra <rajendra.patel@qburst.com>
2023-08-03 16:55:49 +0530

    back to basics

--- src/pages/menupage/home.js
+++ src/pages/menupage/home.js
@@ -3,20 +3,17 @@ import NavBar from "./navbar";
 import "./styles.css";
 
 
-const HomePage = ()=>{
-   
-    
-        return (
-            
-            <div>
-                <NavBar/>
-                <h1 className="homeMessage">Welcome to home page</h1>
-            </div>
-        );
-    
-    
-    
-       
-}
+
+const HomePage = () => {
+  
+  
+    return (
+      <div>
+        <NavBar />
+        <h1 className="homeMessage">Welcome to home page</h1>
+      </div>
+    );
+  
+};
 
 export default HomePage;

a725569c97c9ee819ccadb93829e364b7f618359
Anurag-kumar <anurag.kumar@qburst.com>
2023-08-09 10:27:59 +0530

    Fixed routing to the form page

--- src/pages/formPage/Layout.js
+++ src/pages/formPage/Layout.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import Page1 from './Page1';
-import PageBody from './PageBody';
 import NavBar from '../menupage/navbar';
 import Header from '../../components/header/Header';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FormList from './FormList'
+
 
 
 const Layout = () => {
@@ -12,14 +10,8 @@ const Layout = () => {
     return (
       <div>
           <NavBar/>
-          <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Page1/>}/>
-            <Route path="/list" element={<FormList/>}/>
-            <Route path="/form" element={<Page1/>}/>
-          </Routes>
-      </BrowserRouter>
-         <PageBody/>
+          {/* <Header/> */}
+          <Page1/>
       </div>
     )
   
